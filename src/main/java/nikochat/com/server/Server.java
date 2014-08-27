558f6fe8b2fa8bf0059e925c568a138cf59654bb
nikolaynest <nnicko850@gmail.com>
2014-08-28 00:10:11 +0300

    time added

--- src/main/java/nikochat/com/server/Server.java
+++ src/main/java/nikochat/com/server/Server.java
@@ -9,21 +9,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 /**
  * Created by nikolay on 23.08.14.
  */
-public class Server /*implements Runnable*/ {
+public class Server {
 
     private ServerSocket server;
-    private Socket socket;
-    private BufferedReader input;
-    private PrintWriter output;
-
     //TODO: WHAT BETTER TO USE: LIST OR MAP, OR HOW TO SAVE USER INFO?
-    private Map<String, ServerThread> map = Collections.synchronizedMap(new HashMap<>());
-    private List<ServerThread> threads = Collections.synchronizedList(new ArrayList<>());
+    private final Map<String, ServerThread> map = Collections.synchronizedMap(new HashMap<>());
+    private final List<ServerThread> threads = Collections.synchronizedList(new ArrayList<>());
 
 
     public Server() {
@@ -35,49 +33,19 @@ public class Server /*implements Runnable*/ {
             e.printStackTrace();
         }
 
-        /** connect socket to server */
-//        new Thread(this).start();
         new Thread(new ServerMenu(this)).start();
 
         while (true) {
             try {
-                socket = server.accept();
-//                input = StreamsManager.createInput(socket, this.getClass());
-//                output = StreamsManager.createOutput(socket, this.getClass());
-
-
-                ServerThread serverThread = new ServerThread(socket);
+                ServerThread serverThread = new ServerThread(server.accept());
                 threads.add(serverThread);
-
                 new Thread(serverThread).start();
-
             } catch (IOException e) {
                 System.out.println("Error accepting client on server");
                 e.printStackTrace();
             }
-
         }
-
     }
-
-//    @Override
-//    public void run() {
-//        while (true) {
-//            try {
-//                socket = server.accept();
-////                input = StreamsManager.createInput(socket, this.getClass());
-////                output = StreamsManager.createOutput(socket, this.getClass());
-//            } catch (IOException e) {
-//                System.out.println("Error accepting client on server");
-//                e.printStackTrace();
-//            }
-//
-//            ServerThread serverThread = new ServerThread(socket);
-//            threads.add(serverThread);
-//            Thread t = new Thread(serverThread);
-//            t.start();
-//        }
-//    }
 
     public void killSocket(String name) {
         //TODO:
@@ -89,46 +57,49 @@ public class Server /*implements Runnable*/ {
         private BufferedReader in;
         private PrintWriter out;
 
-        //todo: возможно, передавать in и out параметрами конструктора
         public ServerThread(Socket socket) {
-
             in = StreamsManager.createInput(socket, this.getClass());
             out = StreamsManager.createOutput(socket, this.getClass());
         }
 
+        private String getTime(){
+            LocalTime time = LocalTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+            return formatter.format(time);
+        }
         @Override
         public void run() {
+
             try {
                 /** первым делом получаю имя нового "клиента" */
                 String name = in.readLine();
-                System.out.println(name + " has joined");
+                String time = getTime();
+                System.out.println(time + "  " + name + " has joined");
                 System.out.println("numbers of users: " + threads.size());
-
                 synchronized (threads) {
                     for (ServerThread st : threads) {
-                        st.out.println(name + " has joined");
+                        st.out.println(time + "  " + name + " has joined");
                     }
                 }
-
                 /** читаю из входящего потока сообщения */
-                String message = "";
                 while (true) {
-                    message = in.readLine();
+                    String message = in.readLine();
+                    time = getTime();
                     if (!message.trim().equals("exit")) {
                         synchronized (threads) {
                             for (ServerThread st : threads) {
-                                st.out.println(message);
+                                st.out.println(time + "  " + message);
 
                             }
                         }
-                        System.out.println(message);
+                        System.out.println(time + "  " + message);
                     } else {
                         synchronized (threads) {
                             for (ServerThread st : threads) {
-                                st.out.println(name + " exit from chat");
+                                st.out.println(time + "  " + name + " exit from chat");
                             }
                         }
-                        System.out.println(name + " exit from chat");
+                        System.out.println(time + "  " + name + " exit from chat");
                         break;
                     }
                 }
@@ -137,8 +108,5 @@ public class Server /*implements Runnable*/ {
                 e.printStackTrace();
             }
         }
-
     }
-
-
 }
